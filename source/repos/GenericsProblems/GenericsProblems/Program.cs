e5d3a205afee72b1f399a62944ac6406bcd6f26e
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2022-12-02 22:32:46 +0530

    [Sartaj] add Maximum Number Integer

---
+++ source/repos/GenericsProblems/GenericsProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace GenericsProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Generics Program....");
+            FindMaximum findingmax = new FindMaximum();
+            findingmax.MaximumInteger(45, 98, 78);
+        }
+    }
+}
