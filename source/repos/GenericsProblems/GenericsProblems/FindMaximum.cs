9803885f09244e793ca782686a38539cfb17a7d6
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2022-12-02 23:23:24 +0530

    [Sartaj] add Refacter By Generics Class

--- source/repos/GenericsProblems/GenericsProblems/FindMaximum.cs
+++ source/repos/GenericsProblems/GenericsProblems/FindMaximum.cs
@@ -6,28 +6,8 @@ using System.Threading.Tasks;
 
 namespace GenericsProblems
 {
-    public class FindMaximum<T> where T : IComparable
+    public class FindMaximum
     {
-        public void MaximumValue(T firstValue, T secondValue, T thirdValue)
-        {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
-            {
-                Console.WriteLine("First Value {0} is greatest of all ", firstValue);
-            }
-            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
-                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
-                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
-            {
-                Console.WriteLine("Second Value {0} is greatest of all ", secondValue);
-            }
-            else
-            {
-                Console.WriteLine("Third Value {0} is greatest of all ", thirdValue);
-            }
-
-        }
         public void MaximumInteger(int firstValue, int secondValue, int thirdValue)
         {
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
@@ -46,7 +26,6 @@ namespace GenericsProblems
             {
                 Console.WriteLine("Third Value {0} is greatest of all ", thirdValue);
             }
-
         }
         public void MaximumFloat(double firstValue, double secondValue, double thirdValue)
         {
